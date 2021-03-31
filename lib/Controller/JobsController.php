1623e218384fcb9c42c8cb02192a2f6a3a0e9434
Léa <ljdroguet@gmail.com>
2021-03-31 18:32:30 +0200

    update jobscontroller

--- lib/Controller/JobsController.php
+++ lib/Controller/JobsController.php
@@ -12,70 +12,27 @@
 
  class JobsController extends Controller {
 
-    private $userId;
     private $connection;
 
 
-    public function __construct(IDBConnection $connection, string $AppName, IRequest $request, $userId){
+    public function __construct(IDBConnection $connection, string $AppName, IRequest $request){
         parent::__construct($AppName, $request);
-        $this->userId = $userId;
         $this->connection = $connection;
     }
 
-    	/**
-	 * get the job object from a row in the db
-	 * COPIED FROM JobList ------------------------------------------
-	 * @param array $row
-	 * @return IJob|null
-	 */
-	private function buildJob($row) {
-		try {
-			try {
-				// Try to load the job as a service
-				/** @var IJob $job */
-				$job = \OC::$server->query($row['class']);
-			} catch (QueryException $e) {
-				if (class_exists($row['class'])) {
-					$class = $row['class'];
-					$job = new $class();
-				} else {
-					// job from disabled app or old version of an app, no need to do anything
-					return null;
-				}
-			}
-
-			$job->setId((int) $row['id']);
-			$job->setLastRun((int) $row['last_run']);
-			$job->setArgument(json_decode($row['argument'], true));
-			return $job;
-
-		} catch (AutoloadNotAllowedException $e) {
-			// job is from a disabled app, ignore
-			return null;
-		}
-	}
-
-	
      /**
       * @NoAdminRequired
       * @NoCSRFRequired
-      * @param string $title
-      * @param string $content
       */
     public function getAll() {
         $query = $this->connection->getQueryBuilder();
 		$query->select('*')
 			->from('jobs');
         $result = $query->execute();
-		$jobs = [];
-        while ($row = $result->fetch()) {
-            $job = $this->buildJob($row);
-            if ($job) {
-                $jobs[] = (array) $job;
-            }
-        }
+		$jobs = $result->fetchAll();
         $result->closeCursor();
-        return new DataResponse($jobs);
+        // DataResponse outputs json array
+		return new DataResponse($jobs);
     }
 
  }
